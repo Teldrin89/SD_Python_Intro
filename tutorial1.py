b5e631836ed8c87dd9dc69b31227407e9bead605
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-10-15 23:34:26 +0200

    updated initial comment and added first line of code

--- tutorial1.py
+++ tutorial1.py
@@ -1,1 +1,14 @@
-# inital comment
+'''
+# initial comment
+### The Python intro repo based on sentdex introduction series ###
+The Python langauge is a veristile language that can be applied to many different
+applications with the only drawback beeing slightly slower than some other languages 
+(like for example C)
+The basic python documentation is rich enough to always start searching for specific
+solution: https://docs.python.org/3/
+'''
+
+# the most basic thing a python can do is a print function
+# this print function will print the text in double quotes (string)
+print("Hello people!")
+print("Just checking")
