db2c6e8b726552609bd0c188fa0f643ff9b89944
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-10-18 15:58:28 +0200

    initial commit with 1st comments for tic-tac-toe game

---
+++ tictactoe.py
@@ -0,0 +1,14 @@
+'''
+The goal of this script is to build a game of tic-tac-toe using python and applying
+some of the basic functions and modules of Python.
+The best way to apporach every new project is to write down the idea in simple 
+steps that can be then followed along during the creation.
+# --- The Game of Tic-Tac-Toe ---
+1. The basic representation of a game board will be a list of lists with size 3x3
+2. The starting point will be represented by 0s and the "x" and "o" will be 
+    represented by 1s and 2s (TODO: think over some functions in the future to
+    represent the "x" and "o")
+3. The user input will be determined by first location and then the symbol
+
+
+'''
